de2cad46bc2601b1db2bb877f66452a221731a64
Artur Rodrigues <arturrrocha494@gmail.com>
2025-08-29 15:32:52 -0300

    Protocolo manchester diferencial e camada fisica completos

--- redes1_trabalho01_202310240/model/CamadaFisicaReceptora.java
+++ redes1_trabalho01_202310240/model/CamadaFisicaReceptora.java
@@ -71,11 +71,9 @@ public class CamadaFisicaReceptora {
   @return <code>int[]</code> quadro decodificado
   ****************************************************************/
   private int[] camadaFisicaReceptoraDecodificacaoManchester(int[] fluxoBrutoDeBits) {
-    //a quantidade de bits uteis recebida no meio de comunicacao eh sempre o drobro
+    //a quantidade de sinais uteis recebida no meio de comunicacao eh sempre o drobro
     //da quantidade de bits da mensagem
     int qtdBitsUteis = ContarBits.quantidadeDeBitsUteis(fluxoBrutoDeBits.clone()) / 2;
-
-    System.out.println("quantidade bits uteis : " + qtdBitsUteis);
 
     //calcula quantos inteiros de 32 bits sao necessarios para transmitir a mensagem
     //como eh necessario arredondar para cima, faz qtdBitsUteis + 31
@@ -134,9 +132,73 @@ public class CamadaFisicaReceptora {
   @return <code>int[]</code> quadro decodificado
   ****************************************************************/
   private int[] camadaFisicaReceptoraDecodificacaoManchesterDiferencial(int[] fluxoBrutoDeBits) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException(
-        "Unimplemented method 'camadaFisicaReceptoraDecodificacaoManchesterDiferencial'");
+    System.out.println("camada fisica receptora: ");
+
+    //a quantidade de sinais uteis recebida no meio de comunicacao eh sempre o drobro
+    //da quantidade de bits da mensagem
+    int qtdBitsUteis = ContarBits.quantidadeDeBitsUteis(fluxoBrutoDeBits.clone()) / 2;
+
+    //calcula quantos inteiros de 32 bits sao necessarios para transmitir a mensagem
+    //como eh necessario arredondar para cima, faz qtdBitsUteis + 31
+    int quadro[] = new int[(qtdBitsUteis + 31) / 32];
+
+    //lê-se os 2 primeiros sinais e armazena como o anteriores
+    int mascara = 3;
+    int sinaisAnteriores = (mascara & (fluxoBrutoDeBits[fluxoBrutoDeBits.length - 1]));
+    fluxoBrutoDeBits[fluxoBrutoDeBits.length - 1] >>>= 2; //avanca para leitura dos proximos 2 sinais
+
+    //estruturas de controle
+    int bitsInseridos = 0; //contador de bits inseridos
+    int indiceQuadro = 0;
+    int bit;
+
+    for (int i = (fluxoBrutoDeBits.length - 1); i >= 0; i--) { //para todos os inteiros do fluxo bruto de bits...
+
+      while (fluxoBrutoDeBits[i] != 0) { // enquanto houver bits significativos no inteiro ...
+
+        quadro[indiceQuadro] <<= 1; //abre espaco para novo bit
+
+        //le os 2 sinais para analise
+        int sinais = mascara & fluxoBrutoDeBits[i];
+        fluxoBrutoDeBits[i] >>>= 2;
+
+        if (sinais == sinaisAnteriores) {
+          //se a dupla de sinais forem iguais, ha 2 possibilidades:
+          //    10 10    ou     01 01
+          //em ambos os casos, HA TRANSICAO, e, portanto, o bit eh o 0
+          bit = 0;
+        } else { // se nao, nao ha transicao, e, portanto, o bit eh o 1
+          bit = 1;
+        }
+
+        sinaisAnteriores = sinais;
+
+        quadro[indiceQuadro] |= bit;
+        bitsInseridos++;
+
+        if (bitsInseridos % 32 == 0) {
+          indiceQuadro++;
+        }
+      }
+    }
+
+    int baixoAlto = 1; //sinal 01
+    //O ULTIMO PAR DE SINAIS LIDOS ESTA NO PADRAO MANCHESTER, pois foi o primeiro bit a ser codificado no dispositivo transmissor
+    if (sinaisAnteriores == baixoAlto)
+      bit = 0;
+    else
+      bit = 1;
+    //abre espaco e adiciona o bit no quadro
+    quadro[indiceQuadro] <<= 1;
+    quadro[indiceQuadro] |= bit;
+
+    System.out.println("quadro: ");
+
+    for (int valor : quadro) {
+      System.out.println(Integer.toBinaryString(valor));
+    }
+
+    return quadro;
   }
 
 }
--- redes1_trabalho01_202310240/model/CamadaFisicaTransmissora.java
+++ redes1_trabalho01_202310240/model/CamadaFisicaTransmissora.java
@@ -130,12 +130,14 @@ public class CamadaFisicaTransmissora {
   }
 
   /**************************************************************** <p>
-  * Metodo: camadaFisicaTransmissoraCodificacaoManchesterDiferencial <p>
-  * Funcao: codificacao do quadro com protocolo manchester diferencial <p>
-  @param quadro quadro a ser codificado
+   * Metodo: camadaFisicaTransmissoraCodificacaoManchesterDiferencial <p>
+   * Funcao: codificacao do quadro com protocolo manchester diferencial <p>
+   @param quadro quadro a ser codificado
   @return <code>int[]</code> fluxo de bits codificados
   ****************************************************************/
   private int[] camadaFisicaTransmissoraCodificacaoManchesterDiferencial(int[] quadro) {
+    System.out.println("camada fisica transmissora: ");
+
     //a codificacao manchester dobra a quantidade de bits uteis transmitidos
     int quantidadeBitsUteis = ContarBits.quantidadeDeBitsUteis(quadro.clone()) * 2;
 
@@ -143,8 +145,77 @@ public class CamadaFisicaTransmissora {
     //como eh necessario arredondar para cima, faz quantidadeBitsUteis + 31
     int[] fluxoBrutoDeBits = new int[(quantidadeBitsUteis + 31) / 32];
 
-    //indice do bit sendo lido
-    int i = 1;
+    //PRIMEIRO - ANALISE DO PRIMEIRO BIT COM BASE NA MANCHESTER
+    int mascara = 1;
+    int bit = 0 | (mascara & quadro[quadro.length - 1]);
+    quadro[quadro.length - 1] >>>= 1;
+
+    int altoBaixo = (1 << 1);
+    int baixoAlto = 1;
+    int inserir; //sinal a inserir no fluxo bruto de bits
+
+    if (bit == 0) {
+      inserir = baixoAlto;
+    } else {
+      inserir = altoBaixo;
+    }
+
+    //insere os sinais no fluxo bruto de bits
+    int indiceFluxo = 0;
+    fluxoBrutoDeBits[indiceFluxo] |= inserir;
+    int sinaisInseridos = 2;
+
+    //para analise da alternancia dos sinais, deve-se analisar o sinal anterior
+    int sinalAnterior = inserir;
+
+    int j = 1; //iterador dos bits do quadro (como o primeiro ja foi lido, inicia-se com 1)
+    for (int i = (quadro.length - 1); i >= 0; i--) { //para cada inteiro do quadro...
+      while (quadro[i] != 0) { //enquanto o inteiro possuir bits significativos...
+        for (; j < 8; j++) { //cara cada 8 bits...
+
+          //abre espaco para mais 2 sinais no fluxo Bruto de bits
+          fluxoBrutoDeBits[indiceFluxo] <<= 2;
+
+          //selecionar bit
+          bit = 0 | (mascara & quadro[i]);
+          quadro[i] >>>= 1;
+
+          //verificar qual dupla de sinais inserir no fluxo de bits
+          if (bit != 0) {
+            //se for 1, manter sinal
+            //portanto,
+            //se os sinais anteriores forem 10, insere-se 01
+            //anteriores forem 01, insere-se 10
+            if (sinalAnterior == altoBaixo) { //se for 10 
+              inserir = baixoAlto;
+            } else {
+              inserir = altoBaixo;
+            }
+          }
+          //se for 0, fazer inversao de sinal
+          //portanto,
+          //se os sinais anteriores forem 10, insere-se 10
+          //se os sinais anteriores forem 01, insere-se 01 
+          //portanto, a dupla de sinais mantem-se a mesma
+
+          fluxoBrutoDeBits[indiceFluxo] |= inserir;
+          sinalAnterior = inserir;
+          sinaisInseridos += 2;
+
+          if (sinaisInseridos % 32 == 0) { //se o inteiro encheu...
+            indiceFluxo++; //passa para o proximo inteiro
+          }
+
+        }
+        j = 0;
+      }
+    }
+
+    System.out.println("fluxo bruto de bits: ");
+    for (int valor : fluxoBrutoDeBits) {
+      System.out.println(Integer.toBinaryString(valor));
+    }
+
     return fluxoBrutoDeBits;
   }
 
